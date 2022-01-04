01956e373d84104b88784e606fd14b6dc8b98112
rabin shrestha <timeportal21@gmail.com>
2022-01-05 00:13:37 +0545

    [add] added filter search and validation for data

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,24 +4,30 @@ import {
   Delete,
   Get,
   Param,
+  ParseUUIDPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private taskService: TasksService) {}
+  constructor(private readonly taskService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.taskService.getTaskWithFilters(filterDto);
+    }
     return this.taskService.getAllTasks();
   }
 
   @Get('/:id')
-  getTaskById(@Param('id') id: string) {
+  getTaskById(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.taskService.getTaskById(id);
   }
 
@@ -39,7 +45,7 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
+  deleteTask(@Param('id', new ParseUUIDPipe()) id: string): void {
     this.taskService.deleteTask(id);
   }
 }
